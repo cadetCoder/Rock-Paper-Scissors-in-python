4acb7840508a0720f4d3d44cc1dff84c85e8d22f
Your Name <darreltoledo@icloud.com>
2023-01-22 20:58:42 +0000

    adding random selections for computer to choose value

--- rps.py
+++ rps.py
@@ -1,3 +1,7 @@
 import random
-
+#takes the user choice from the input choice lists
 user_action = input("Enter a choice(rock, paper, scissors):\t")
+
+#Make the computer choice by using random.choice() function
+possible_actions = ["rock", "paper", "scissors"]
+computer_action = random.choice(possible_actions)
