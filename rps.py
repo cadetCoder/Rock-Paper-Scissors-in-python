45bdaa2626f964114a7fa885335c99d91e10fef0
Your Name <darreltoledo@icloud.com>
2023-01-22 21:00:23 +0000

    added print of user and computer actions

--- rps.py
+++ rps.py
@@ -1,7 +1,7 @@
 import random
 #takes the user choice from the input choice lists
 user_action = input("Enter a choice(rock, paper, scissors):\t")
-
 #Make the computer choice by using random.choice() function
 possible_actions = ["rock", "paper", "scissors"]
 computer_action = random.choice(possible_actions)
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
